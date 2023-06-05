5604e2a391e33c8db3e1312376ecc6da67e7519f
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-06 00:24:57 +0530

    fixed npm error, gave some styling structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,16 @@ import { CssBaseline } from "@mui/material";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <NavBar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
           <Route path="/movie/:id" element={<MovieInformation />} />
           <Route path="/actors/:id" element={<Actors />} />
