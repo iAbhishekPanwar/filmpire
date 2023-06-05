3f53596cb5b392f80b43c6c74738a4f0dbaac06e
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-05 06:14:23 +0530

    structured all files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
+import { CssBaseline } from "@mui/material";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
