127a1ebf3a725919be5361a9e3fafdef3ea878f6
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-06 10:50:19 +0530

    fixing search functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,7 +18,7 @@ import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import { useTheme } from "@mui/material/styles";
 // import { useTheme } from "@emotion/react";
-import { Sidebar } from "..";
+import { Search, Sidebar } from "..";
 
 const NavBar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -44,7 +44,7 @@ const NavBar = () => {
           <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
             {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
-          {!isMobile && "Search..."}
+          {!isMobile && <Search />}
           <div>
             {!isAuthenticated ? (
               <Button color="inherit" onClick={() => {}}>
@@ -67,7 +67,7 @@ const NavBar = () => {
               </Button>
             )}
           </div>
-          {isMobile && "Search..."}
+          {isMobile && <Search />}
         </Toolbar>
       </AppBar>
       <div>
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,19 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
+// const page = 1;
 
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
   endpoints: (builder) => ({
+    //Get Movies by Searching
+    if(searchQuery) {
+      return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+    },
     /*Get Movie Genres*/
     getGenres: builder.query({
       query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
     }),
     // Get movies by [type]
     getMovies: builder.query({
-      query: ({ genreIdOrCategoryName, page }) => {
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
         //Movie by Category
         if (
           genreIdOrCategoryName &&
